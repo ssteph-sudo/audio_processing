511b7b59cb118946053e3008740552754ab6d498
brepowell <85724630+brepowell@users.noreply.github.com>
2023-11-26 16:18:28 -0800

    Deleted java classifier folder

--- java classifier/Save_Model.java
+++
@@ -1,7 +0,0 @@
-// Modified from https://waikato.github.io/weka-wiki/serialization/
-// Save the model (after training and testing) for loading and deployment
-import weka.core.SerializationHelper;
-
-public void save_model(Classifier cls, String filename){
-    weka.core.SerializationHelper.write(filename, cls);
-}
--- java classifier/Use_Model.java
+++
@@ -1,8 +0,0 @@
-// Modified from https://waikato.github.io/weka-wiki/serialization/
-// Load the model for use
-import weka.core.SerializationHelper;
-
-public Classifier load_model(Classifier cls, String filename){
-    Classifier cls = (Classifier) weka.core.SerializationHelper.read(filename);
-    return cls;
-}
