95dffdbef21b6d45a03ff7ff672d5a47b616f713
ssteph-sudo <77129616+ssteph-sudo@users.noreply.github.com>
2023-11-27 18:41:57 -0800

    Edited GUi so it now creates the CSV.

--- feature_extraction/Feature_Extractor.py
+++ feature_extraction/Feature_Extractor.py
@@ -1,86 +1,63 @@
-import csv
-import pandas as pd
-import numpy as np
 import os
-from Feature_Extraction_Zero_Crossing_Rate import extract_zero_crossing
-from Feature_Extraction_Spectral_Centroid import extract_spectral_centroid
-from Feature_Extraction_Avg_Energy import extract_avg_energy
+import pandas as pd
 
+from .Feature_Extraction_Zero_Crossing_Rate import extract_zero_crossing
+from .Feature_Extraction_Spectral_Centroid import extract_spectral_centroid
+from .Feature_Extraction_Avg_Energy import extract_avg_energy
 
-speech_file_path = "../audio/speech/"
-music_file_path = "../audio/music/"
+def generate_file_list(dir_path):
+    print(f"Generating file list from directory: {dir_path}")
+    file_list = []
 
-# To prepare for machine learning process, the data may be formatted as below:
-# filename, f1, f2, …, fn, label
-
-def generate_file_list():
-    
-    speechFiles = os.listdir(speech_file_path)
-    musicFiles = os.listdir(music_file_path)
-
-    speechCount = int(len(speechFiles))
-    musicCount = int(len(musicFiles))
-
-    fileList = []
-
-    for file in range(1, speechCount + 1):
-        fileName = "sp" + str(file) + ".wav"
-        fileList.append(fileName)
-
-    
-    for file in range(1, musicCount + 1):
-        fileName = "mu" + str(file) + ".wav"
-        fileList.append(fileName)
-
-    print(len(fileList))
-    return fileList
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith('.wav'):
+                full_path = os.path.join(root, file)
+                file_list.append(full_path)  # Append the full path
+    print(f"Found {len(file_list)} files.")
+    return file_list
 
 def pipeline(path, row):
-    # Audio Feature 1
-    avg_energy = extract_avg_energy(path)
-    row.append(avg_energy)
+    print(f"Extracting features from {path}")
+    try:
+        avg_energy = extract_avg_energy(path)
+        row.append(avg_energy)
 
-    # Audio Feature 2
-    spectral_centroid_avg = extract_spectral_centroid(path)
-    row.append(spectral_centroid_avg)
+        spectral_centroid_avg = extract_spectral_centroid(path)
+        row.append(spectral_centroid_avg)
 
-    # Audio Feature 3
-    zero_crossing_feature = extract_zero_crossing(path)
-    row.append(zero_crossing_feature)
+        zero_crossing_feature = extract_zero_crossing(path)
+        row.append(zero_crossing_feature)
+    except Exception as e:
+        print(f"Error during feature extraction from {path}: {e}")
 
-def generate_csv():
+def determine_label(filename):
+    return "yes" if "mu" in filename else "no"
+
+def generate_csv(dir_path):
+    print(f"Path received in generate_csv: {dir_path}")
     data = []
-    files = generate_file_list()
+    files = generate_file_list(dir_path)
     headerList = ["fileName", "Avg_Energy", "Spectral_Centroid", "Zero_Crossing", "Label"]
 
-    for file in range(len(files)):
-        row = []
-        filename = files[file]  # For all files in the file list
-        row.append(filename)    # Grab the name of file
+    for filename in files:
+        row = [filename]
+        path = os.path.join(dir_path, filename)
 
-        path = ""
-        label = ""
-
-        # label has value “yes” if this file is a music clip and “no” if it is a speech file.
-        if "mu" in filename:
-            path = music_file_path + filename
-            label = "yes"
-        else:
-            path = speech_file_path + filename
-            label = "no"
-
-        # Pass the file through the pipeline to extract features
-        pipeline(path, row)
-
-        # Label each audio file with a ground truth label
+        label = determine_label(filename) 
         row.append(label)
 
-        # Add each row to the dataset
+        pipeline(path, row)
+
         data.append(row)
 
-    # Save the features as a dataframe and then a csv file
-    df = pd.DataFrame(data)
-    print(df.shape)
-    df.to_csv('features.csv', mode='w', index=False, header=headerList)
+    df = pd.DataFrame(data, columns=headerList)
+    print(f"DataFrame shape: {df.shape}")
+    if df.empty:
+        print("Warning: The DataFrame is empty. No data extracted.")
+    else:
+        print(df.head())  # Print the first few rows of the DataFrame
 
-generate_csv()
+    output_path = 'features.csv'
+    df.to_csv(output_path, mode='w', index=False)
+    print(f"Features saved to CSV file at: {output_path}")
